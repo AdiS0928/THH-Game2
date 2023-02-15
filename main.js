3428bf549ed0da7db49b7c22e3cd9a30af9cb2fc
hsggamer <55681300+hsggamer@users.noreply.github.com>
2023-02-15 10:29:42 +0400

    commit

--- main.js
+++ main.js
@@ -195,5 +195,5 @@ app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
-
+  module.exports = app;
 
